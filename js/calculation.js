fa285ce8f73e3e269470f9c93773bab15ddb7be0
masud <abdullahalmasud01737906772@gmail.com>
2022-02-17 23:04:32 +0600

    fifth commit

--- js/calculation.js
+++ js/calculation.js
@@ -4,26 +4,21 @@ document.getElementById('calculate-button').addEventListener('click', function (
     const incomeInput = document.getElementById('income-input');
     const incomeInputText = incomeInput.value;
     const monthlyIncomeInput = parseFloat(incomeInputText);
-    //console.log('income',monthlyIncomeInput);
 
     // get monthly Food expenses
     const foodExpensesInput = document.getElementById('food-expenses');
     const foodExpensesInputText = foodExpensesInput.value;
     const monthlyFoodExpensesInput = parseFloat(foodExpensesInputText);
-    //console.log('food',monthlyFoodExpensesInput);
 
     // get monthly rent expenses
     const rentExpensesInput = document.getElementById('rent-expenses');
     const rentExpensesInputText = rentExpensesInput.value;
     const monthlyRentExpensesInput = parseFloat(rentExpensesInputText);
-    //console.log('rent', monthlyRentExpensesInput);
 
     //get monthly colthes expenses
     const clothesExpensesInput = document.getElementById('clothes-expenses');
     const clothesExpensesInputText = clothesExpensesInput.value;
     const monthlyClothesExpensesInput = parseFloat(clothesExpensesInputText);
-    //console.log('clothes', monthlyClothesExpensesInput);
-
 
     // total monthly expenses
     const totalExpenses = document.getElementById('total-expeses');
@@ -31,7 +26,6 @@ document.getElementById('calculate-button').addEventListener('click', function (
     const oldExpensesAmount = parseFloat(oldExpensesText);
     const totalExpensesAmount = oldExpensesAmount + monthlyFoodExpensesInput + monthlyRentExpensesInput + monthlyClothesExpensesInput;
     totalExpenses.innerText = totalExpensesAmount;
-
 
     // update balance
     const balance = document.getElementById('balance');
@@ -48,19 +42,63 @@ document.getElementById('calculate-button').addEventListener('click', function (
     clothesExpensesInput.value = '';
 })
 
-
 // handle save button
 document.getElementById('save-button').addEventListener('click', function () {
-    //get the monthly income
-    const incomeInput = document.getElementById('income-input');
-    const incomeInputText = incomeInput.value;
-    const monthlyIncomeInput = parseFloat(incomeInputText);
+    // get the total experses 
+    const totalExpenses = document.getElementById('total-expeses');
+    const totalExpensesText = totalExpenses.innerText;
+    const oldTotalExpenses = parseFloat(totalExpensesText);
+
+    // get the total balance 
+    const expensesAfterBalance = document.getElementById('balance');
+    const expensesAfterBalanceText = expensesAfterBalance.innerText;
+    const oldExpenceAfterBalance = parseFloat(expensesAfterBalanceText);
+
+    // get the total monthly salary
+    const totalMonthlySalary = oldTotalExpenses + oldExpenceAfterBalance;
 
     //get the save amount parsent
     const saveInput = document.getElementById('save-input');
     const saveAmountText = saveInput.value;
     const saveAmount = parseFloat(saveAmountText);
-    const saveAmountParsent = (monthlyIncomeInput/100) * saveAmount;
-    
+
+    // get the save amount 
+    const savingAmount = document.getElementById('saving-amount');
+    const oldSavingInputText = savingAmount.innerText;
+    const oldSavingAmount = parseFloat(oldSavingInputText);
+    const saveAmountParsent = (totalMonthlySalary / 100) * saveAmount;
+    const newSavingAmount = oldSavingAmount + saveAmountParsent;
+    savingAmount.innerText = newSavingAmount;
+
+    // get remaing balance
+    const newRemaingBalance = oldExpenceAfterBalance - newSavingAmount;
+
+    const remaingBalance = document.getElementById('remaing-balance');
+    const oldRemaingBalanceText = remaingBalance.innerText;
+    const oldRemaingBalance = parseFloat(oldRemaingBalanceText);
+    const leatestRemaingBalance = newRemaingBalance + oldRemaingBalance;
+    remaingBalance.innerText = leatestRemaingBalance;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // clear deposit input field
+    saveInput.value = '';
 
 })
